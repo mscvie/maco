054357ab89e0e7519b498ccdbdfba857268bb037
mscvie <martin.schretter@gmail.com>
2024-06-28 10:27:04 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/maco/", 
   title: "Simple VitePress Site",
   description: "Training Page for Cloud Computing Course",
   themeConfig: {
